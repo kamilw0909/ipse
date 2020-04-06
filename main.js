ee3de9501a84b109d526e8796d9374b072feafbc
kamilw0909 <kwardach88@gmail.com>
2020-04-06 21:12:45 +0200

    Contact form added without sending to google form page.

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
 $('.navbar-nav>a').on('click', function(){
     $('.navbar-collapse').collapse('hide');
 });
+
+$('#contact-form').css('display', 'none');
+
+$('#contact-form').submit(function (e) {
+    e.preventDefault();
+    console.log('Formularz wysłany')
+});
+
+$('#form-button').click(function(){
+    $('#contact-form').css('display', 'block');
+    $(this).css('display', 'none');
+});
