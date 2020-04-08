020e85d1dd5cc37531cc82cf99eef3e2abd6ceac
kamilw0909 <kwardach88@gmail.com>
2020-04-08 13:02:22 +0200

    Prapare js for ajax

--- main.js
+++ main.js
@@ -11,5 +11,39 @@ $('#form-button').click(function(){
 
 $('#contact-form').submit(function (e) {
     e.preventDefault();
-    console.log('Formularz wysłany')
+
+    var iframe = document.createElement('iframe');
+    var uniqueString = 'UNIQUE_IFRAME_NAME_ON_IPSE_PAGE';
+    document.body.appendChild(iframe);
+    iframe.style.display = 'none';
+    iframe.contentWindow.name = uniqueString;
+
+    $(this).target = uniqueString;
+
+    var name = $('#name').val();
+    var email = $('#email').val();
+    var phone = $('#phone').val() || 'brak';
+    var massege = $('#massege').val();
+
+    $.ajax({
+        url: 'https://docs.google.com/forms/d/e/1FAIpQLSf_Q5kUCuQiqxtEzGKbovrP3YPKwZi-G4jky9-85XWEvYwLCQ/formResponse',
+        data: {
+            'entry.2005620554' :name,
+            'entry.1045781291': email,
+            'entry.1166974658': phone,
+            'entry.839337160': massege
+        },
+        type: 'POST',
+        dataType: 'xml',
+        statusCode: {
+            0: function(e) {
+                console.log('błąd' + ' ', e);
+            },
+            200: function() {
+                console.log('sukces');
+            }
+        }
+
+
+    })
 });
